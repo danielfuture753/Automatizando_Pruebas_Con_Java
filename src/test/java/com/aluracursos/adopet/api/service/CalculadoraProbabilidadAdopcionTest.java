a84f03244e9c2c4312a44b0a1c3b10893810adff
DANIEL SALAZAR GARCIA <daniel_hal1_@hotmail.com>
2025-01-30 20:08:38 -0600

    SECCION 1 FINALIZADA

---
+++ src/test/java/com/aluracursos/adopet/api/service/CalculadoraProbabilidadAdopcionTest.java
@@ -0,0 +1,66 @@
+package com.aluracursos.adopet.api.service;
+
+import com.aluracursos.adopet.api.dto.RegistroMascotaDto;
+import com.aluracursos.adopet.api.dto.RegistroRefugioDto;
+import com.aluracursos.adopet.api.model.Mascota;
+import com.aluracursos.adopet.api.model.ProbabilidadAdopcion;
+import com.aluracursos.adopet.api.model.Refugio;
+import com.aluracursos.adopet.api.model.TipoMascota;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraProbabilidadAdopcionTest {
+
+
+    @Test
+    void escenario01(){
+        //Edad 4 años y 4 kilos -> ALTA
+
+        RegistroMascotaDto registroMascotaDto= new RegistroMascotaDto(
+                TipoMascota.GATO,
+                "Miau",
+                "Siamés",
+                4,
+                "Gris",
+                4.0f
+        );
+        Refugio refugio = new Refugio(new RegistroRefugioDto(
+                "Refugio feliz",
+                "94999999999",
+                "refugiofeliz@email.com.br"
+        ));
+        Mascota mascota = new Mascota(registroMascotaDto, refugio);
+
+        CalculadoraProbabilidadAdopcion calculadora = new CalculadoraProbabilidadAdopcion();
+        ProbabilidadAdopcion probabilidad = calculadora.calcular(mascota);
+
+        Assertions.assertEquals(ProbabilidadAdopcion.ALTA, probabilidad);
+    }
+
+
+    @Test
+    void escenario02(){
+        //Edad 15 años y 4 kilos -> MEDIA
+
+        RegistroMascotaDto registroMascotaDto= new RegistroMascotaDto(
+                TipoMascota.GATO,
+                "Miau",
+                "Siamés",
+                15,
+                "Gris",
+                4.0f
+        );
+        Refugio refugio = new Refugio(new RegistroRefugioDto(
+                "Refugio feliz",
+                "94999999999",
+                "refugiofeliz@email.com.br"
+        ));
+        Mascota mascota = new Mascota(registroMascotaDto, refugio);
+
+        CalculadoraProbabilidadAdopcion calculadora = new CalculadoraProbabilidadAdopcion();
+        ProbabilidadAdopcion probabilidad = calculadora.calcular(mascota);
+
+        Assertions.assertEquals(ProbabilidadAdopcion.MEDIA, probabilidad);
+    }
+
+}
